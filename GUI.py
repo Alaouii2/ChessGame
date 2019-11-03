b51e13605635bfd231787902fdbceeeaaa2e0f74
Ilias <ilias.alaoui@student.ecp.fr>
2019-11-03 23:39:52 +0100

    Too many bugs, will fix later (help)

--- GUI.py
+++ GUI.py
@@ -1,5 +1,18 @@
 import pygame
 import board
+import move
+import player
+import copy
+
+
+def opposite_color(color):
+    if color == 'b':
+        opp = 'w'
+    elif color == 'w':
+        opp = 'b'
+    else:
+        opp = '0'
+    return opp
 
 SIZE = W, H = 512, 512
 CAPTION = 'ChessGame with Python3'
@@ -18,8 +31,14 @@ clc = pygame.time.Clock()
 # Board
 b = board.Board()
 
-all_rects = [[pygame.Rect(j*SQUAREW, i*SQUAREH, SQUAREW, SQUAREH) for j in range(8)] for i in range(8)]
-
+all_rects = [[pygame.Rect(j * SQUAREW, i * SQUAREH, SQUAREW, SQUAREH) for j in range(8)] for i in range(8)]
+hitboxes = {}
+for i in [0, 1, 6, 7]:
+    for j in range(8):
+        if i in [0, 1]:
+            hitboxes[(i, j, 'b')] = pygame.Rect(j * SQUAREW, i * SQUAREH, SQUAREW, SQUAREH)
+        else:
+            hitboxes[(i, j,  'w')] = pygame.Rect(j * SQUAREW, i * SQUAREH, SQUAREW, SQUAREH)
 
 def drawboard(bd):
     for i in range(8):
@@ -31,7 +50,7 @@ def drawboard(bd):
 
 
 # pieces
-white_king = pygame.transform.scale(pygame.image.load('assets/2000px-Chess_Pieces_Sprite_01.png'), (SQUAREW , SQUAREH))
+white_king = pygame.transform.scale(pygame.image.load('assets/2000px-Chess_Pieces_Sprite_01.png'),  (SQUAREW, SQUAREH))
 white_queen = pygame.transform.scale(pygame.image.load('assets/2000px-Chess_Pieces_Sprite_02.png'), (SQUAREW, SQUAREH))
 white_bishop = pygame.transform.scale(pygame.image.load('assets/2000px-Chess_Pieces_Sprite_03.png'), (SQUAREW, SQUAREH))
 white_knight = pygame.transform.scale(pygame.image.load('assets/2000px-Chess_Pieces_Sprite_04.png'), (SQUAREW, SQUAREH))
@@ -46,48 +65,107 @@ black_pawn = pygame.transform.scale(pygame.image.load('assets/2000px-Chess_Piece
 
 
 def draw_pieces(bd):
-    for i in range(8):
+    for i in [0, 1, 6, 7]:
         for j in range(8):
             sq = b.squares[i][j]
-            rect = all_rects[i][j]
             if sq.p.name == 'k' and sq.p.color == 'w':
-                screen.blit(white_king, rect)
+                screen.blit(white_king, hitboxes[(i, j, sq.p.color)])
             if sq.p.name == 'q' and sq.p.color == 'w':
-                screen.blit(white_queen, rect)
+                screen.blit(white_queen, hitboxes[(i, j, sq.p.color)])
             if sq.p.name == 'b' and sq.p.color == 'w':
-                screen.blit(white_bishop, rect)
+                screen.blit(white_bishop, hitboxes[(i, j, sq.p.color)])
             if sq.p.name == 'c' and sq.p.color == 'w':
-                screen.blit(white_knight, rect)
+                screen.blit(white_knight, hitboxes[(i, j, sq.p.color)])
             if sq.p.name == 'r' and sq.p.color == 'w':
-                screen.blit(white_rook, rect)
+                screen.blit(white_rook, hitboxes[(i, j, sq.p.color)])
             if sq.p.name == 'p' and sq.p.color == 'w':
-                screen.blit(white_pawn, rect)
+                screen.blit(white_pawn, hitboxes[(i, j, sq.p.color)])
             if sq.p.name == 'k' and sq.p.color == 'b':
-                screen.blit(black_king, rect)
+                screen.blit(black_king, hitboxes[(i, j, sq.p.color)])
             if sq.p.name == 'q' and sq.p.color == 'b':
-                screen.blit(black_queen, rect)
+                screen.blit(black_queen, hitboxes[(i, j, sq.p.color)])
             if sq.p.name == 'b' and sq.p.color == 'b':
-                screen.blit(black_bishop, rect)
+                screen.blit(black_bishop, hitboxes[(i, j, sq.p.color)])
             if sq.p.name == 'c' and sq.p.color == 'b':
-                screen.blit(black_knight, rect)
+                screen.blit(black_knight, hitboxes[(i, j, sq.p.color)])
             if sq.p.name == 'r' and sq.p.color == 'b':
-                screen.blit(black_rook, rect)
+                screen.blit(black_rook, hitboxes[(i, j, sq.p.color)])
             if sq.p.name == 'p' and sq.p.color == 'b':
-                screen.blit(black_pawn, rect)
+                screen.blit(black_pawn, hitboxes[(i, j, sq.p.color)])
 
 
 b.initiate()
+player1 = player.Player('Player1', 'w')
+player2 = player.Player('Player2', 'b')
 
+dragging = False
+offset_x, offset_y, start_x, start_y, finish_x, finish_y, rect, init_rect = 0, 0, None, None, None, None, None, None
 run = 1
+turn = 1
 
 while run:
+    if turn % 2 == 1:
+        plyer = player1
+    else:
+        plyer = player2
+    color = plyer.color
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = 0
-        # if event.type == pygame.MOUSEBUTTONDOWN:
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            print('Click')
+            if event.button == 1:
+                mouse_x, mouse_y = pygame.mouse.get_pos()
+                for i in [0, 1, 6, 7]:
+                    for j in range(8):
+                        try:
+                            rect = hitboxes[(i, j, color)]
+                        except KeyError:
+                            continue
+                        if rect.collidepoint(mouse_x, mouse_y):
+                            print('collide')
+                            dragging = True
+                            init = start_x, start_y = i, j
+                            offset_x = rect.x - mouse_x
+                            offset_y = rect.y - mouse_y
 
+        elif event.type == pygame.MOUSEMOTION:
+            if dragging:
+                mouse_x, mouse_y = pygame.mouse.get_pos()
+                rect.x = mouse_x + offset_x
+                rect.y = mouse_y + offset_y
+        elif event.type == pygame.MOUSEBUTTONUP:
+            print('Drop')
+            if dragging:
+                dragging = False
+                for i in range(8):
+                    for j in range(8):
+                        sq = all_rects[i][j]
+                        if rect.collidepoint(sq.centerx, sq.centery):
+                            print('yes')
+                            finish_x, finish_y = i, j
+                            m = move.Move(b, (start_x, start_y), (finish_x, finish_y))
+                            if m.is_legal():
+                                print('legal')
+                                rect.center = sq.center
+                                b.update(m)
+                                if b.stalemate(opposite_color(plyer.color)):
+                                    print('Stalemate! Tie')
+                                    run = 0
+                                if b.checkmate(opposite_color(plyer.color)):
+                                    print('Checkmate! {} wins'.format(plyer.name))
+                                    run = 0
+                                turn += 1
+                            else:
+                                print(init, rect.x, rect.y)
+                                rect.center = all_rects[start_x][start_y].center
+    # print(rect)
     drawboard(b)
     draw_pieces(b)
+    # for i in range(8):
+    #     for j in range(8):
+    #         pygame.draw.rect(screen,(200, 0, 0), hitboxes[(i, j]))
+
     pygame.display.flip()
     clc.tick(FPS)
 
